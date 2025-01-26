257f649ed19d7f7758d542a679ca27cd31d6e42d
mielesantos <mielesnts@gmail.com>
2025-01-26 14:41:36 -0300

    feat: add rota para gera image

--- api/routers.py
+++ api/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from udemy_ia.api.v1.text_router import router as text_router
+from udemy_ia.api.v1.text_router import router as text_router  # type: ignore
+from udemy_ia.api.v1.vision_router import router as image_router  # type: ignore
 
 router = APIRouter()
 router.include_router(text_router, prefix="/api/v1", tags=["CHAT"])
+router.include_router(image_router, prefix="/api/v1", tags=["Image"])
